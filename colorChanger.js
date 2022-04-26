fda829c07af9d32f7263ecec3f4b6d2a0bc60167
joakimdeak <joakimdeak@hotmail.com>
2022-04-26 13:03:27 +0200

    change color of all matches

--- colorChanger.js
+++ colorChanger.js
@@ -5,21 +5,13 @@ const observer = new MutationObserver(changeColor);
 observer.observe(target, config);
 
 function changeColor() {
-  const openMatches = document.querySelectorAll('[title="Status: Open"]');
-  if (openMatches.length > 0) {
-    const prOpen = openMatches[0];
-    prOpen.setAttribute(
-      "style",
-      "background-color:rgb(35, 134, 54) !important"
-    );
+  const matches = document.querySelectorAll('[title="Status: Open"]');
+  for (const el of matches) {
+    el.setAttribute('style', 'background-color:rgb(35, 134, 54) !important');
   }
 
-  const iconMatches = document.querySelectorAll(
-    '[class*="octicon-git-pull-request open"]'
-  );
-  if (iconMatches.length > 0) {
-    for (match of iconMatches) {
-      match.setAttribute("style", "color:rgb(63, 185, 80) !important");
-    }
+  const iconMatches = document.querySelectorAll('[class*="octicon-git-pull-request open"]');
+  for (match of iconMatches) {
+    match.setAttribute('style', 'color:rgb(63, 185, 80) !important');
   }
 }
