3864f177e9457c093c9d815cc5f11a6662d60563
joakimdeak <joakimdeak@hotmail.com>
2022-06-15 14:55:50 +0200

    match icons on recent activity

--- colorChanger.js
+++ colorChanger.js
@@ -10,8 +10,8 @@ function changeColor() {
     el.setAttribute('style', 'background-color:rgb(35, 134, 54) !important');
   }
 
-  const iconMatches = document.querySelectorAll('[class*="octicon-git-pull-request open"]');
-  for (match of iconMatches) {
+  const recentActivityMatches = document.querySelectorAll('[aria-label="Open pull request"]');
+  for (match of [...iconMatches, ...recentActivityMatches]) {
     match.setAttribute('style', 'color:rgb(63, 185, 80) !important');
   }
 }
