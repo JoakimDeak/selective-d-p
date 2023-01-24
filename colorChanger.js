f3de85c9a029e47e3aead53d097cbf9ab56b942c
joakimdeak <joakimdeak@hotmail.com>
2023-01-24 11:11:13 +0100

    fix

--- colorChanger.js
+++ colorChanger.js
@@ -5,14 +5,9 @@ const observer = new MutationObserver(changeColor);
 observer.observe(target, config);
 
 function changeColor() {
-  const matches = document.querySelectorAll('[title="Status: Open"]');
-  for (const el of matches) {
-    el.setAttribute('style', 'background-color:rgb(35, 134, 54) !important');
-  }
+  const elementMatches = document.querySelectorAll('[title="Status: Open"]');
+  elementMatches.forEach((match) => match.setAttribute('style', 'background-color:rgb(35, 134, 53) !important'));
 
-  const iconMatches = document.querySelectorAll('[class*="octicon-git-pull-request open"]');
-  const recentActivityMatches = document.querySelectorAll('[aria-label="Open pull request"]');
-  for (match of [...iconMatches, ...recentActivityMatches]) {
-    match.setAttribute('style', 'color:rgb(63, 185, 80) !important');
-  }
+  const iconMatches = document.querySelectorAll('[class*="color-fg-open"], [aria-label="Open pull request"]');
+  iconMatches.forEach((match) => match.setAttribute('style', 'color:rgb(63, 185, 80) !important'));
 }
